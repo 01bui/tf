4310e610a4dcf275e40a5c5c77a7106eb9312e55
Vbui <01bui@cardinalmail.cua.edu>
2016-10-22 20:56:39 -0400

    label

--- tfCensus.py
+++ tfCensus.py
@@ -1,10 +1,16 @@
+# https://www.tensorflow.org/versions/r0.11/tutorials/wide/index.html#tensorflow-linear-model-tutorial
+
 import tempfile
 import urllib
+
+#The dataset we'll be using is the Census Income Dataset.
+# You can download the training data and test data manually or use code like this:
 train_file = tempfile.NamedTemporaryFile()
 test_file = tempfile.NamedTemporaryFile()
 urllib.urlretrieve("https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data", train_file.name)
 urllib.urlretrieve("https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test", test_file.name)
 
+#Once the CSV files are downloaded, let's read them into Pandas dataframes.
 import pandas as pd
 COLUMNS = ["age", "workclass", "fnlwgt", "education", "education_num",
            "marital_status", "occupation", "relationship", "race", "gender",
@@ -12,3 +18,9 @@ COLUMNS = ["age", "workclass", "fnlwgt", "education", "education_num",
            "income_bracket"]
 df_train = pd.read_csv(train_file, names=COLUMNS, skipinitialspace=True)
 df_test = pd.read_csv(test_file, names=COLUMNS, skipinitialspace=True, skiprows=1)
+
+#Since the task is a binary classification problem, we'll construct a label column named "label"
+# whose value is 1 if the income is over 50K, and 0 otherwise.
+LABEL_COLUMN = "label"
+df_train[LABEL_COLUMN] = (df_train["income_bracket"].apply(lambda x: ">50K" in x)).astype(int)
+df_test[LABEL_COLUMN] = (df_test["income_bracket"].apply(lambda x: ">50K" in x)).astype(int)
